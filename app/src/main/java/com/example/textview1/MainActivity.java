3bf0424e57eb18a87bdb8fad89a691668562c4b6
ay4748 <ay4748@bs.amalnet.k12.il>
2023-12-19 09:06:27 +0200

    final

--- app/src/main/java/com/example/textview1/MainActivity.java
+++ app/src/main/java/com/example/textview1/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.textview1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    TextView tV;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tV = findViewById(R.id.tV);
+    }
+
+    public void dodge(View view) {
+        tV.setTextColor(Color.RED);
+        tV.setText("Oh, yea, I’ve been clicked!");
     }
 }
